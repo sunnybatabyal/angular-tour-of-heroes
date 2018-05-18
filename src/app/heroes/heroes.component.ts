8f4758d308842d4b7ab373cb69c22c714aecb8a3
Sunny Batabyal <batabyalsunny@gmail.com>
2018-05-19 00:39:28 +0530

    Showing hero details on click of hero name from list.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,12 +9,15 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Spiderman'
-  };
+  private heroes = HEROES;
+
+  private selectedHero: Hero;
 
   constructor() { }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   ngOnInit() {
   }
